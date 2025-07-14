9f3746af6d48c0143e2627ee90fbf0bd43f94e26
Mitch Negus <mitchell.negus.57@gmail.com>
2025-07-13 18:27:35 -0600

    Extend valid Python version range

--- noxfile.py
+++ noxfile.py
@@ -17,7 +17,7 @@ PYTHON_PROJECT_FILES = [
 # --- TESTING ---
 #
 
-PYTHON_VERSIONS = ["3.10", "3.11", "3.12"]
+PYTHON_VERSIONS = ["3.10", "3.11", "3.12", "3.13"]
 
 
 @nox.session(name="clean-coverage")
